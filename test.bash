333e1b165f7d22cf6845081ac101575fe7d74841
Tetsushi Kawabata <s21c1037mo@s.chibakoudai.jp>
2022-11-03 12:32:28 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,16 +2,23 @@
 # SPDX-FileCopyrightText: 2022 Tetsuhi Kawabata
 #SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "${out}" = "" ]  || ng ${LINENO}
 
 out=$(echo | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "${out}" = "" ]  || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
